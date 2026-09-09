2c2bb6e6a039617bdc36fd20b97fc3e55e21fa4a
Shivam <s4387mishra@gmail.com>
2026-09-09 23:47:03 +0530

    Add files via upload

---
+++ andGate/andGate.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.09.2026 15:16:23
+// Design Name: 
+// Module Name: andGate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module andGate(input a, input b, output c);
+    assign c = a&b;
+endmodule
